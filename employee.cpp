d1ddca3bce8f1ba3e17bbd1f621768cfdb4ba3ee
Ayushmaan Singh Jamwal <100258159+ASJ01@users.noreply.github.com>
2023-03-02 01:12:24 +0530

    Update employee.cpp

--- employee.cpp
+++ employee.cpp
@@ -1,29 +1,34 @@
 #include<iostream>
 using namespace std;
 struct Employee
-{char name[60];
- char empid[30];
- char houseno[80];
- char area[20];
- char city[20];
- char state[20];
+{
+    char name[60];
+    char empid[30];
+    char houseno[80];
+    char area[20];
+    char city[20];
+    char state[20];
 };
 int main()
-{Employee s;
-cout << "Name of the Employee:";
-cin >> s.name;
-cout << "Enter the Employee Id:";
-cin >> s.empid;
-cout << "Enter the House Number";
-cin>> s.dob;
-cout<< "Branch of the Student:";
-cin>> s.branch;
-cout<< "Total Marks of the Student:";
-cin>> s.marks;
+{
+    Employee s;
+    cout << "Name of the Employee : ";
+    cin >> s.name;
+    cout << "Enter the Employee Id : ";
+    cin >> s.empid;
+    cout << "Enter the House Number : ";
+    cin>> s.houseno;
+    cout<< "Enter the area:";
+    cin>> s.area;
+    cout<< "Enter the city : ";
+    cin>> s.city;
+    cout<< "Enter the state : ";
+    cin>> s.state;
 
-cout<<"Name:"<< s.name<<endl;
-cout<<"Roll Number:"<<s.rollno<<endl;
-cout<<"Date Of Birth:"<< s.dob<<endl;
-cout<<"Branch:"<< s.branch<<endl;
-cout<<"Marks:"<< s.marks<<endl;
-return 0;}
+    cout << "\nDisplaying Information \n";
+
+    cout<<"Name:"<< s.name<<endl;
+    cout<<"Employee ID:"<<s.empid<<endl;
+    cout<<"Address:"<< s.houseno<<","<<s.area<<","<<s.city<<","<<s.state<<endl;
+    return 0;
+}
